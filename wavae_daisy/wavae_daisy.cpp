d28130af1a0c8c43da54f64ec8529b75339c427d
Simon Nakane Marcil <simon.nakanemarcil@mail.mcgill.ca>
2023-02-03 14:37:20 -0500

    Switches between two sine

--- wavae_daisy/wavae_daisy.cpp
+++ wavae_daisy/wavae_daisy.cpp
@@ -29,7 +29,7 @@ void AudioCallback(AudioHandle::InputBuffer in, AudioHandle::OutputBuffer out, s
 int main(void)
 {
     hw.Init();
-
+    bool flag = 0;
     auto step = static_cast<float>(2 * M_PI / WAVE_LEN);
     for (size_t i = 0; i < WAVE_LEN; i++)
     {
@@ -38,7 +38,8 @@ int main(void)
 
     for (size_t i = 0; i < WAVE_LEN; i++)
     {
-        tri.samples[i] = i * (2/WAVE_LEN) + -1;
+        // tri.samples[i] = i * ((float)2.0 / (float)WAVE_LEN) + -1;
+        tri.samples[i] = 0.5 * sinf(i * step);
     }
 
     // step = 2.f / WAVE_LEN;
@@ -53,5 +54,15 @@ int main(void)
     hw.StartAudio(AudioCallback);
     while (1)
     {
+        System::Delay(5000);
+        if (!flag)
+        {
+            osc.SetWave(&tri);
+        }
+        else
+        {
+            osc.SetWave(&sine);
+        }
+        flag = !flag;
     }
 }
