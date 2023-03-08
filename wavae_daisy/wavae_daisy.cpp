d2ec698174f1b34e0a0ffe0a464f189b60c755ca
Simon Nakane Marcil <simon.nakanemarcil@mail.mcgill.ca>
2023-03-08 15:31:45 -0500

    usb midi interface added

--- wavae_daisy/wavae_daisy.cpp
+++ wavae_daisy/wavae_daisy.cpp
@@ -1,11 +1,10 @@
 #include "daisy_patch_sm.h"
-#include "daisysp.h"
 
 using namespace daisy;
 using namespace patch_sm;
-using namespace daisysp;
 
 DaisyPatchSM hw;
+MidiUsbHandler midi;
 
 void AudioCallback(AudioHandle::InputBuffer in, AudioHandle::OutputBuffer out, size_t size)
 {
@@ -20,8 +19,41 @@ void AudioCallback(AudioHandle::InputBuffer in, AudioHandle::OutputBuffer out, s
 int main(void)
 {
 	hw.Init();
-	hw.SetAudioBlockSize(4); // number of samples handled per callback
+
+	/** Initialize USB Midi */
+	MidiUsbHandler::Config midi_cfg;
+	midi_cfg.transport_config.periph = MidiUsbTransport::Config::INTERNAL;
+	midi.Init(midi_cfg);
+
+	/** Configure audio handler */
 	hw.SetAudioSampleRate(SaiHandle::Config::SampleRate::SAI_48KHZ);
 	hw.StartAudio(AudioCallback);
-	while(1) {}
+
+	while (1)
+	{
+		/** Listen to MIDI for new changes */
+		midi.Listen();
+
+		/** When there are messages waiting in the queue... */
+		while (midi.HasEvents())
+		{
+			/** Pull the oldest one from the list... */
+			auto msg = midi.PopEvent();
+			switch (msg.type)
+			{
+			case NoteOn:
+			{
+				/** and change the frequency of the oscillator */
+				auto note_msg = msg.AsNoteOn();
+				if (note_msg.velocity != 0)
+					hw.SetLed(true);
+				else
+					hw.SetLed(false);
+			}
+			break;
+			default:
+				hw.SetLed(false);
+			}
+		}
+	}
 }
