da1d18c88a5ee4614d1e4606d5069e14194818f9
Simon Nakane Marcil <simon.nakanemarcil@mail.mcgill.ca>
2023-03-28 23:42:31 -0400

    Squashed commit of the following:
    commit a769e9c44e92dc4138ee4b282e4645bcde68ba47
    Author: Simon Nakane Marcil <simon.nakanemarcil@mail.mcgill.ca>
    Date:   Tue Mar 28 22:17:47 2023 -0400
    
        using uint8_t
    
    commit 9be13bb05285b12a6342e3a6fb53b792e784c259
    Author: Simon Nakane Marcil <simon.nakanemarcil@mail.mcgill.ca>
    Date:   Mon Mar 20 23:54:00 2023 -0400
    
        simple midi interface
    
    commit d2ec698174f1b34e0a0ffe0a464f189b60c755ca
    Author: Simon Nakane Marcil <simon.nakanemarcil@mail.mcgill.ca>
    Date:   Wed Mar 8 15:31:45 2023 -0500
    
        usb midi interface added

--- wavae_daisy/wavae_daisy.cpp
+++ wavae_daisy/wavae_daisy.cpp
@@ -7,38 +7,34 @@ using namespace patch_sm;
 using namespace daisysp;
 
 DaisyPatchSM hw;
+MidiUsbHandler midi;
 WaveOsc osc;
+
 Wave DSY_SDRAM_BSS sine;
 Wave DSY_SDRAM_BSS saw;
-
 constexpr float TWO_PI_RECIP = 1.0f / TWOPI_F;
+
+uint8_t noteOn;
+Adsr adsr;
 
 void AudioCallback(AudioHandle::InputBuffer in, AudioHandle::OutputBuffer out, size_t size)
 {
 	hw.ProcessAllControls();
 
-	float coarse_knob = hw.GetAdcValue(CV_1);
-	float coarse = fmap(coarse_knob, 36.f, 96.f);
-
-	float voct_cv = hw.GetAdcValue(CV_5);
-	float voct = fmap(voct_cv, 0.f, 60.f);
-
-	float midi_nn = fclamp(coarse + voct, 0.f, 127.f);
-	float freq = mtof(midi_nn);
-
-	osc.SetFreq(freq);
-
 	for (size_t i = 0; i < size; i++)
 	{
-		float sig = osc.Process();
-		OUT_L[i] = sig;
-		OUT_R[i] = sig;
+		OUT_L[i] = OUT_R[i] = osc.Process() * adsr.Process(noteOn != -1);
 	}
 }
 
 int main(void)
 {
 	hw.Init();
+
+	/** Initialize USB Midi */
+	MidiUsbHandler::Config midi_cfg;
+	midi_cfg.transport_config.periph = MidiUsbTransport::Config::INTERNAL;
+	midi.Init(midi_cfg);
 
 	float phase = TWOPI_F / (float)WAVE_LEN;
 	for (size_t i = 0; i < WAVE_LEN; i++)
@@ -56,9 +52,60 @@ int main(void)
 			saw.samples[i] = (i - t) * step - 1;
 	}
 
+	/** Configure audio handler */
+	hw.SetAudioSampleRate(SaiHandle::Config::SampleRate::SAI_48KHZ);
+
+	/** Initialize our tone */
 	osc.Init(hw.AudioSampleRate(), &saw);
+	noteOn = -1;
+	adsr.Init(hw.AudioSampleRate());
+
 	hw.StartAudio(AudioCallback);
 	while (1)
 	{
+		/** Listen to MIDI for new changes */
+		midi.Listen();
+
+		/** When there are messages waiting in the queue... */
+		while (midi.HasEvents())
+		{
+			/** Pull the oldest one from the list... */
+			auto msg = midi.PopEvent();
+			switch (msg.type)
+			{
+			case NoteOn:
+			{
+				/** and change the frequency of the oscillator */
+				auto note_msg = msg.AsNoteOn();
+				if (note_msg.velocity != 0)
+				{
+					noteOn = note_msg.note;
+					osc.SetFreq(daisysp::mtof(noteOn));
+					hw.SetLed(true);
+				}
+				else
+				{
+					if (noteOn == note_msg.note)
+					{
+						noteOn = -1;
+						hw.SetLed(false);
+					}
+				}
+			}
+			break;
+			case NoteOff:
+			{
+				auto note_msg = msg.AsNoteOff();
+				if (noteOn == note_msg.note)
+				{
+					noteOn = -1;
+					hw.SetLed(false);
+				}
+			}
+			break;
+			default:
+				break;
+			}
+		}
 	}
 }
